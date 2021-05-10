c12008efbf3b4428ec5fae1c1b197e2c3d414491
maxim ashuev <maximashuev@gmail.com>
2021-05-10 14:15:57 -0500

    Modifications

--- Tests/jenkins_is_up_test.py
+++ Tests/jenkins_is_up_test.py
@@ -8,20 +8,5 @@ class TestJenkinsTitle:
 
     def test_jenkins_title(self):
         self.driver.get("http://localhost:8080/")
-        # WebDriverWait(self.driver, 30).until(EC.presence_of_element_located((By.ID, 'j_username')))
         print("*************************************")
-        # # print(os.environ)
-        # InitialAdminPassword = os.environ.get('InitialAdminPassword')
-        # print(InitialAdminPassword)
-        # # asd= os.system('docker exec jenkins cat /var/jenkins_home/secrets/initialAdminPassword')
-        # # with open ('/home/travis/apt-get-update.log', "r") as file:
-        # #     print(file.read())
-        # #
-        # print("*************************************")
-        # self.driver.find_element_by_id('security-token').send_keys(os.system('docker exec jenkins cat /var/jenkins_home/secrets/initialAdminPassword'))
-        # time.sleep(5)
-        # print(self.driver.find_element_by_xpath('//*[@id="main-panel"]/form/div[1]/div/div/div/div[2]/div/p[2]/small/code').text)
         print(self.driver.title, "!!!!!!!!!!!!!!!!!!")
-        # with open("/var/jenkins_home/secrets/initialAdminPassword", "r") as file:
-        #     jenkins_password = file.read()
-        # print(jenkins_password)
