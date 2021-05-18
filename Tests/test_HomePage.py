32f75d05274cda8f36171949481ed5585112ad24
maxim ashuev <maximashuev@gmail.com>
2021-05-18 16:25:57 -0500

    Add Pages folder Add TestData folder

--- Tests/test_HomePage.py
+++ Tests/test_HomePage.py
@@ -1,6 +1,6 @@
 from Pages.BasePage import BasePage
 from Pages.HomePage import HomePage
-
+import time
 
 class TestHomePage:
 
@@ -9,5 +9,9 @@ class TestHomePage:
 
     def test_new_item(self):
         new_item = HomePage(self.driver)
+        time.sleep(3)
         new_item.click(HomePage.NEW_ITEM)
+        time.sleep(3)
         new_item.go_to_page('http://localhost:8080/')
+        time.sleep(3)
+
