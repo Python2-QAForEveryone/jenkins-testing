2cf71223c50cc82773ef27dd1dca8a33efe47bcd
Mikhail Mir <microf2000@rambler.ru>
2021-05-18 13:45:17 -0700

    added 2 method in BasePage.py, test_dashboard_menu_anchor_is_clickable_tc_001, locators_HomePage.py

--- Tests/test_HomePage.py
+++ Tests/test_HomePage.py
@@ -1,5 +1,6 @@
 from Pages.BasePage import BasePage
 from Pages.HomePage import HomePage
+from Tests.locators_HomePage import DashboardPageLocators
 
 
 class TestHomePage:
@@ -11,3 +12,6 @@ class TestHomePage:
         new_item = HomePage(self.driver)
         new_item.click(HomePage.NEW_ITEM)
         new_item.go_to_page('http://localhost:8080/')
+
+    def test_dashboard_menu_anchor_is_clickable_tc_001(self):
+        assert BasePage.is_clickable(DashboardPageLocators.DASHBOARD_MENU_ANCHOR)
