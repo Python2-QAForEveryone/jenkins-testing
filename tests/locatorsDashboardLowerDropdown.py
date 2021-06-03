f1c36c83453660d96ca167285719ade179757dad
Yevgeniya Rudenko <janetevg@gmail.com>
2021-06-03 13:49:19 -0700

    fixed empty lines

--- tests/locatorsDashboardLowerDropdown.py
+++ tests/locatorsDashboardLowerDropdown.py
@@ -1,7 +1,5 @@
 from selenium.webdriver.common.by import By
 
-from config.TestData import TestData
-
 class DashboardLowerDropdownPageLocators:
 
     DASHBOARD_LINK = (By.XPATH, "//a[text()='Dashboard']")
@@ -10,4 +8,3 @@ class DashboardLowerDropdownPageLocators:
     RIGHT_ARROW_SELECTOR_ALL = (By.XPATH, '//a[@href="/view/all/"]')
     RIGHT_ARROW_SELECTOR_ALL_VISIBLE = (By.ID, 'breadcrumb-menu')
     NEW_ITEM = (By.XPATH, ("//li[contains(@id,'yui-gen')]/a[contains(@href,'newJob')]"))
-
