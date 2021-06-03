64d3aabe16cbadaa9a507f167ff63123e2047610
Yevgeniya Rudenko <janetevg@gmail.com>
2021-06-03 07:42:17 -0700

    added test_dashboard_down_arrow_is_visible_and_clickable added test_dashboard_lower_dropdown_items_clickable added hover_over_element hover_over_and_click

--- tests/locatorsDashboardLowerDropdown.py
+++ tests/locatorsDashboardLowerDropdown.py
@@ -9,4 +9,5 @@ class DashboardLowerDropdownPageLocators:
     RIGHT_ARROW_SELECTOR = (By.CLASS_NAME, 'children')
     RIGHT_ARROW_SELECTOR_ALL = (By.XPATH, '//a[@href="/view/all/"]')
     RIGHT_ARROW_SELECTOR_ALL_VISIBLE = (By.ID, 'breadcrumb-menu')
+    NEW_ITEM = (By.XPATH, ("//li[contains(@id,'yui-gen')]/a[contains(@href,'newJob')]"))
 
--- tests/test_dashboard_lower_dropdown_page.py
+++ tests/test_dashboard_lower_dropdown_page.py
@@ -7,3 +7,15 @@ class TestDashboardLowerDropdownPage:
         driver = DashboardLowerDropdownPage(self.driver)
         assert driver.is_clickable(DashboardLowerDropdownPageLocators.DASHBOARD_LINK)
         assert driver.is_visible(DashboardLowerDropdownPageLocators.DASHBOARD_LINK)
+
+    def test_dashboard_down_arrow_is_visible_and_clickable(self):
+        driver = DashboardLowerDropdownPage(self.driver)
+        driver.hover_over_element(DashboardLowerDropdownPageLocators.DASHBOARD_LINK)
+        assert driver.is_visible(DashboardLowerDropdownPageLocators.MENU_SELECTOR)
+        assert driver.is_clickable(DashboardLowerDropdownPageLocators.MENU_SELECTOR)
+
+    def test_dashboard_lower_dropdown_items_clickable(self):
+        driver = DashboardLowerDropdownPage(self.driver)
+        driver.hover_and_click_element(DashboardLowerDropdownPageLocators.DASHBOARD_LINK,
+                                            DashboardLowerDropdownPageLocators.MENU_SELECTOR)
+        assert driver.is_clickable(DashboardLowerDropdownPageLocators.NEW_ITEM)
