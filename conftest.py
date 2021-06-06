20a296febd11ef4433941bb9e94c149afb5401ff
maxim ashuev <maximashuev@gmail.com>
2021-06-06 15:24:42 -0500

    Conftest 1.2

--- conftest.py
+++ conftest.py
@@ -66,7 +66,7 @@ def init_driver(request):
         raise Exception('driver is not found')
 
     driver.get(TD.BASE_URL)
-    WebDriverWait(driver, 90).until(EC.presence_of_element_located((By.ID, 'j_username'))).send_keys(TD.LOGIN)
+    WebDriverWait(driver, 120).until(EC.presence_of_element_located((By.ID, 'j_username'))).send_keys(TD.LOGIN)
     driver.find_element(By.CSS_SELECTOR, 'input[name="j_password"]').send_keys(TD.PASSWORD)
     driver.find_element(By.CSS_SELECTOR, 'input[name="Submit"]').click()
     request.cls.driver = driver
