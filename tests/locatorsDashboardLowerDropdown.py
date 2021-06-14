146994e4cdaa5a9103dedba950bd03f81eae7abd
Yevgeniya Rudenko <janetevg@gmail.com>
2021-06-14 14:38:09 -0700

    fixed locator

--- tests/locatorsDashboardLowerDropdown.py
+++ tests/locatorsDashboardLowerDropdown.py
@@ -12,7 +12,7 @@ class DashboardLowerDropdownLocators:
     NEW_ITEM = (By.XPATH, ("//li[contains(@id,'yui-gen')]/a[contains(@href,'newJob')]"))
     PEOPLE = (By.XPATH, ("//li[contains(@id,'yui-gen')]/a[contains(@href,'People')]"))
     BUILD_HISTORY = (By.XPATH, ("//li[contains(@id,'yui-gen')]/a[contains(@href,'builds')]"))
-    MANAGE_JENKINS = (By.XPATH, ("//li[contains(@id,'yui-gen')]/a[contains(text(),' Manage Jenkins')]"))
+    MANAGE_JENKINS = (By.XPATH, ("//li[contains(@id,'yui-gen')]/a[contains(text(),'Manage Jenkins')]"))
     MY_VIEWS = (By.XPATH, ("//li[contains(@id,'yui-gen')]/a[contains(@href,'my-views')]"))
     LOCKABLE_RESOURCES = (By.XPATH, ("//li[contains(@id,'yui-gen')]/a[contains(@href,'lockable-resources')]"))
     NEW_VIEW = (By.XPATH, ("//li[contains(@id,'yui-gen')]/a[contains(@href,'newView')]"))
