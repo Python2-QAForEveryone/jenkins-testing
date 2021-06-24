0d2bce96ea825c2a9b65a9c17acf1a2dce391866
dsk1975 <dsko1975@gmail.com>
2021-06-23 20:58:03 -0700

    Develop files for GlobalToolConfiguration page.

--- config/TestData.py
+++ config/TestData.py
@@ -1,5 +1,4 @@
 class TestData:
-
     BASE_URL = 'http://localhost:8080/'
-    LOGIN = 'dsko1975'
-    PASSWORD = 'h887A4Q2'
+    LOGIN = 'user'
+    PASSWORD = '12345678'
--- pages/GlobalToolConfiguration.py
+++
@@ -1,9 +0,0 @@
-from pages.BasePage import BasePage
-from config.TestData import TestData
-from selenium.webdriver.common.by import By
-
-class GlobalToolConfiguration(BasePage):
-    TITLE = 'Global Tool Configuration'
-
-    def __init__(self, driver):
-        super().__init__(driver)
