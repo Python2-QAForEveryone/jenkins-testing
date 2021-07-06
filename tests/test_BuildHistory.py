2d451cda259c8397b7e1a4debea7ef11aa26b305
NadiaK <nadia.kataeva@gmail.com>
2021-07-05 21:14:10 -0700

    Refactored names of locators to capital letters

--- tests/test_BuildHistory.py
+++ tests/test_BuildHistory.py
@@ -8,22 +8,22 @@ class TestBuildHistory:
 
     def test_buildhistoryurlvisible(self):
         bhp_driver = BuildHistoryPage(self.driver)
-        bhp_driver.go_to_page(BuildHistoryPage.page_url)
+        bhp_driver.go_to_page(BuildHistoryPage.PAGE_URL)
         curr_url = str(bhp_driver.get_currenturl())
-        assert (BuildHistoryPage.href in curr_url)
+        assert (BuildHistoryPage.BHP_HREF in curr_url)
 
     @pytest.mark.skip
     def test_build_history_logo_visible(self):
         bhp_driver = BuildHistoryPage(self.driver)
-        bhp_driver.go_to_page(BuildHistoryPage.page_url)
-        assert bhp_driver.is_visible(BuildHistoryPage.bhpage_image)
+        bhp_driver.go_to_page(BuildHistoryPage.PAGE_URL)
+        assert bhp_driver.is_visible(BuildHistoryPage.BHPAGE_IMAGE)
 
     def test_build_history_table_header_visible(self):
         bhp_driver = BuildHistoryPage(self.driver)
-        bhp_driver.go_to_page(BuildHistoryPage.page_url)
-        assert bhp_driver.is_visible(BuildHistoryPage.table_header)
+        bhp_driver.go_to_page(BuildHistoryPage.PAGE_URL)
+        assert bhp_driver.is_visible(BuildHistoryPage.BHP_TABLE_HEADER1)
 
     def test_build_history_header_build_btn_clickable(self):
         bhp_driver = BuildHistoryPage(self.driver)
-        bhp_driver.go_to_page(BuildHistoryPage.page_url)
-        assert bhp_driver.is_clickable(BuildHistoryPage.project_status_buttn)
+        bhp_driver.go_to_page(BuildHistoryPage.PAGE_URL)
+        assert bhp_driver.is_clickable(BuildHistoryPage.PROJECT_STATUS_BUTTN)
