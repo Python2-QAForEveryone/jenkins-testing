4278a2d218858b7ee1cc3b87ffd807c249bc8b34
Vadym Koroliuk <vkqa@outlook.com>
2021-07-22 16:02:59 -0700

    Added FolderPage.py, test_folder_page.py, create test_create_folder

---
+++ tests/test_folder_page.py
@@ -0,0 +1,17 @@
+from pages.FolderPage import FolderPage
+from pages.FolderPage import FolderPageLocator, URLLocators
+
+
+class TestFolderPage:
+
+    def test_create_folder(self):
+        driver = FolderPage(self.driver)
+        driver.go_to_page(URLLocators.URL_FOLDER_CREATE)
+
+        driver.do_send_keys(FolderPageLocator.ITEM_NAME, FolderPage.name)
+        driver.get_element(FolderPageLocator.LINK_FOLDER).click()
+        driver.get_element(FolderPageLocator.OK_BUTTON).click()
+
+        assert driver.get_title() == FolderPage.TITLE
+
+
