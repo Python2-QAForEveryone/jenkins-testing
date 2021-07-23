edbe1f383f349b49dbb580fd45c612fd7ae59979
Vadym Koroliuk <30486462+Wis13@users.noreply.github.com>
2021-07-22 22:27:10 -0700

    JN-27 Create a Folder (#49)
     Added FolderPage.py, test_folder_page.py, create test_create_folder
     Added test_name_folder_more_255_ch

---
+++ pages/FolderPage.py
@@ -0,0 +1,35 @@
+import random
+import string
+from config.TestData import TestData
+from selenium.webdriver.common.by import By
+from pages.BasePage import BasePage
+
+
+class FolderPage(BasePage):
+    """
+    initialized driver
+    added moving to the BasePage after log in
+    """
+
+    def __init__(self, driver):
+        super().__init__(driver)
+
+    name = (''.join(random.choice(string.ascii_letters) for i in range(10)))
+    long_name = (''.join(random.choice(string.ascii_letters) for i in range(256)))
+
+    TITLE = f"{name} Config [Jenkins]"
+    WRONG_TITLE = "Jenkins [Jenkins]"
+
+
+class FolderPageLocator:
+    LINK_NEW_ITEM = (By.XPATH, "//a[@title='New Item']")
+    ITEM_NAME = (By.ID, 'name')
+    LINK_FOLDER = (By.CLASS_NAME, 'com_cloudbees_hudson_plugins_folder_Folder')
+    OK_BUTTON = (By.XPATH, '//span[@class="yui-button primary large-button"]')
+    WRONG_REQUEST = (By.XPATH,
+                     '//div[@id="error-description"]/h2[contains(text(),"A problem occurred ")]')
+
+
+class URLLocators:
+    URL_FOLDER_CREATE = TestData.BASE_URL + 'view/all/newJob'
+    URL_PEOPLE_MANAGE = TestData.BASE_URL + f'job/{FolderPage.name}/configure'
