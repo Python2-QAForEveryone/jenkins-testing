5d907bb066bda93a5281ad1b1589cae1f89e1076
Vadym Koroliuk <vkqa@outlook.com>
2021-07-27 15:58:35 -0700

    Added remove created folder after successful attempt. Added test_name_only_three_or_more_dots

--- pages/FolderPage.py
+++ pages/FolderPage.py
@@ -37,10 +37,13 @@ class FolderPage(BasePage):
     name_whitespace = (''.join(random.choice(white_spaces)))
 
     TITLE = f"{name} Config [Jenkins]"
+    TITLE_JOB = f"All [{name}] [Jenkins]"
+    TITLE_DIGITS_JOB = f"All [{name_digits}] [Jenkins]"
     TITLE_DIGITS = f"{name_digits} Config [Jenkins]"
     TITLE_START_DOT = f"{name_start_dot} Config [Jenkins]"
+    TITLE_START_DOT_JOB = f"All [{name_start_dot}] [Jenkins]"
     TITLE_INSIDE_DOT = f"{name_inside_dot} Config [Jenkins]"
-    TITLE_THREE_START_DOT = f"{name_only_three_or_more_dots} Config [Jenkins]"
+    TITLE_INSIDE_DOT_JOB = f"All [{name_inside_dot}] [Jenkins]"
 
     WRONG_TITLE = "Jenkins [Jenkins]"
 
@@ -51,6 +54,7 @@ class FolderPageLocator:
     LINK_FOLDER = (By.CLASS_NAME, 'com_cloudbees_hudson_plugins_folder_Folder')
     OK_BUTTON = (By.ID, 'ok-button')
     OK_BUTTON_DISABLED = (By.XPATH, "//button[@id='ok-button'][@class='disabled']")
+    SAVE_BUTTON = (By.ID, 'yui-gen15-button')
     WRONG_REQUEST = (By.XPATH,
                      '//div[@id="error-description"]/h2[contains(text(),"A problem occurred ")]')
     ITEM_NAME_INVALID = (By.XPATH, "//div[@id='itemname-invalid'][contains(text(),'is an unsafe character')]")
@@ -58,6 +62,8 @@ class FolderPageLocator:
     ITEM_NAME_REQUIRED = (By.XPATH, "//div[@id='itemname-required'][contains(text(),'This field cannot be empty')]")
     ITEM_NAME_NOT_ALLOWED = (By.XPATH, "//div[@id='itemname-invalid'][contains(text(),'is not an allowed name')]")
     ERROR_PAGE_NONAME = (By.XPATH, "//div[@id='main-panel']/p[contains(text(),'No name is specified')]")
+    LINK_DELETE_FOLDER = (By.XPATH, "//a[@title='Delete Folder']")
+    BUTTON_YES = (By.ID, 'yui-gen1-button')
 
 
 class URLLocators:
--- tests/test_folder_page.py
+++ tests/test_folder_page.py
@@ -1,5 +1,5 @@
 from pages.FolderPage import FolderPage
-from pages.FolderPage import FolderPageLocator, URLLocators
+from pages.FolderPage import FolderPageLocator
 
 
 class TestFolderPage:
@@ -9,15 +9,17 @@ class TestFolderPage:
         driver.do_send_keys(FolderPageLocator.ITEM_NAME, FolderPage.name)
         driver.get_element(FolderPageLocator.LINK_FOLDER).click()
         driver.get_element(FolderPageLocator.OK_BUTTON).click()
-
         assert driver.get_title() == FolderPage.TITLE
+        driver.get_element(FolderPageLocator.SAVE_BUTTON).click()
+        assert driver.get_title() == FolderPage.TITLE_JOB
+        driver.get_element(FolderPageLocator.LINK_DELETE_FOLDER).click()
+        driver.get_element(FolderPageLocator.BUTTON_YES).click()
 
     def test_name_folder_more_255_ch(self):
         driver = FolderPage(self.driver)
         driver.do_send_keys(FolderPageLocator.ITEM_NAME, FolderPage.long_name)
         driver.get_element(FolderPageLocator.LINK_FOLDER).click()
         driver.get_element(FolderPageLocator.OK_BUTTON).click()
-
         assert driver.get_title() == FolderPage.WRONG_TITLE
         assert driver.is_element_present(FolderPageLocator.WRONG_REQUEST)
 
@@ -25,7 +27,6 @@ class TestFolderPage:
         driver = FolderPage(self.driver)
         driver.do_send_keys(FolderPageLocator.ITEM_NAME, FolderPage.name_start_special_ch)
         driver.get_element(FolderPageLocator.LINK_FOLDER).click()
-
         assert driver.is_element_present(FolderPageLocator.OK_BUTTON_DISABLED)
         assert driver.is_element_present(FolderPageLocator.ITEM_NAME_INVALID)
 
@@ -33,11 +34,8 @@ class TestFolderPage:
         driver = FolderPage(self.driver)
         driver.do_send_keys(FolderPageLocator.ITEM_NAME, FolderPage.name_inside_special_ch)
         driver.get_element(FolderPageLocator.LINK_FOLDER).click()
-
         assert driver.is_element_present(FolderPageLocator.ITEM_NAME_INVALID)
-
         driver.get_element(FolderPageLocator.OK_BUTTON).click()
-
         assert driver.is_element_present(FolderPageLocator.ERROR_PAGE)
 
     def test_name_folder_digits(self):
@@ -45,14 +43,16 @@ class TestFolderPage:
         driver.do_send_keys(FolderPageLocator.ITEM_NAME, FolderPage.name_digits)
         driver.get_element(FolderPageLocator.LINK_FOLDER).click()
         driver.get_element(FolderPageLocator.OK_BUTTON).click()
-
         assert driver.get_title() == FolderPage.TITLE_DIGITS
+        driver.get_element(FolderPageLocator.SAVE_BUTTON).click()
+        assert driver.get_title() == FolderPage.TITLE_DIGITS_JOB
+        driver.get_element(FolderPageLocator.LINK_DELETE_FOLDER).click()
+        driver.get_element(FolderPageLocator.BUTTON_YES).click()
 
     def test_name_folder_empty(self):
         driver = FolderPage(self.driver)
         driver.do_send_keys(FolderPageLocator.ITEM_NAME, FolderPage.name_empty)
         driver.get_element(FolderPageLocator.LINK_FOLDER).click()
-
         assert driver.is_element_present(FolderPageLocator.OK_BUTTON_DISABLED)
         assert driver.is_element_present(FolderPageLocator.ITEM_NAME_REQUIRED)
 
@@ -61,22 +61,27 @@ class TestFolderPage:
         driver.do_send_keys(FolderPageLocator.ITEM_NAME, FolderPage.name_start_dot)
         driver.get_element(FolderPageLocator.LINK_FOLDER).click()
         driver.get_element(FolderPageLocator.OK_BUTTON).click()
-
         assert driver.get_title() == FolderPage.TITLE_START_DOT
+        driver.get_element(FolderPageLocator.SAVE_BUTTON).click()
+        assert driver.get_title() == FolderPage.TITLE_START_DOT_JOB
+        driver.get_element(FolderPageLocator.LINK_DELETE_FOLDER).click()
+        driver.get_element(FolderPageLocator.BUTTON_YES).click()
 
     def test_name_folder_inside_dot(self):
         driver = FolderPage(self.driver)
         driver.do_send_keys(FolderPageLocator.ITEM_NAME, FolderPage.name_inside_dot)
         driver.get_element(FolderPageLocator.LINK_FOLDER).click()
         driver.get_element(FolderPageLocator.OK_BUTTON).click()
-
         assert driver.get_title() == FolderPage.TITLE_INSIDE_DOT
+        driver.get_element(FolderPageLocator.SAVE_BUTTON).click()
+        assert driver.get_title() == FolderPage.TITLE_INSIDE_DOT_JOB
+        driver.get_element(FolderPageLocator.LINK_DELETE_FOLDER).click()
+        driver.get_element(FolderPageLocator.BUTTON_YES).click()
 
     def test_name_folder_only_one_or_two_dot(self):
         driver = FolderPage(self.driver)
         driver.do_send_keys(FolderPageLocator.ITEM_NAME, FolderPage.name_only_one_or_two_dot)
         driver.get_element(FolderPageLocator.LINK_FOLDER).click()
-
         assert driver.is_element_present(FolderPageLocator.OK_BUTTON_DISABLED)
         assert driver.is_element_present(FolderPageLocator.ITEM_NAME_NOT_ALLOWED)
 
@@ -85,5 +90,11 @@ class TestFolderPage:
         driver.do_send_keys(FolderPageLocator.ITEM_NAME, FolderPage.name_whitespace)
         driver.get_element(FolderPageLocator.LINK_FOLDER).click()
         driver.get_element(FolderPageLocator.OK_BUTTON).click()
-
         assert driver.is_element_present(FolderPageLocator.ERROR_PAGE_NONAME)
+
+    def test_name_only_three_or_more_dots(self):
+        driver = FolderPage(self.driver)
+        driver.do_send_keys(FolderPageLocator.ITEM_NAME, FolderPage.name_only_three_or_more_dots)
+        driver.get_element(FolderPageLocator.LINK_FOLDER).click()
+        driver.get_element(FolderPageLocator.OK_BUTTON).click()
+        assert driver.is_element_present(FolderPageLocator.WRONG_REQUEST)
